617f41ad6aff5ded5b5ca9bf6372e53b07c6edb1
Tayla Monet <taymjenk@gmail.com>
2019-12-16 11:28:12 -0500

    my solution: brute force

--- Two-Sum.js
+++ Two-Sum.js
@@ -14,3 +14,27 @@
 // second loop "j" starting at i + 1 up until givenNumsArray.length -1
 // add each value at current index i & j
 // if i + j === target return [i, j]
+
+const nums = [2, 7, 11, 15]
+const target = 9
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    let intOne = nums[i]
+    for (let k = i + 1; k < nums.length; k++) {
+      let intTwo = nums[k]
+      if (intOne + intTwo === target) {
+          return [i, k]
+      }
+    }
+  }
+}
+
+twoSum(nums, target)
+
+// Time complexity : O(n^2)
+// For each element, we try to find its complement by looping through the rest of array which takes O(n) time. Therefore, the time complexity is O(n^2)
+
+// Space complexity : O(1)
+
+//return intOne + intTwo === target && [i, k]
