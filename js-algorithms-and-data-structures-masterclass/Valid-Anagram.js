9fc08e499b76441657b5c3dd63c744592b896251
Tayla Monet <taymjenk@gmail.com>
2020-01-26 18:23:32 -0500

    re-org files & adding additional problem solving patterns

---
+++ js-algorithms-and-data-structures-masterclass/Valid-Anagram.js
@@ -0,0 +1,14 @@
+//RESTATE...
+// write a function, takes two strings
+// determine if the second string is an anagram of the first string, returns true or false
+// anagram: word, phrase, or name formed by rearranging the letters of another
+// ex: iceman --> cinema
+
+//EXAMPLES...
+//'aaz', 'zza' --> false
+// '', '' --> true
+// 'anagram', 'nagaram' --> true
+// 'rat', 'car' --> false
+// 'awesome', 'awesom' --> false
+
+//APROACH...
