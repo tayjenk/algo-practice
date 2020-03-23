f43e54da2e84d6cf59556e03f18bfffb52a6a999
Tayla Monet <taymjenk@gmail.com>
2020-03-23 10:34:48 -0400

    min path sum second solution

--- dynamic-programming/minimum-path-sum.js
+++ dynamic-programming/minimum-path-sum.js
@@ -21,8 +21,12 @@ const minPathSum = function(grid) {
   }, 0)
   minMatrix.push(topRow)
 
-  for(let r = 1; r < grid.length; r++) {
-    for (let c = 0; c < grid[0].length; c++) {
+  //get matrix dimensions
+  const row = grid.length
+  const column = grid[0].length
+
+  for(let r = 1; r < row; r++) {
+    for (let c = 0; c < column; c++) {
       //console.log(minMatrix)
       let gridSpot = grid[r][c]
       //console.log('gridSpot', gridSpot, 'grid[r][c-1]', !!grid[r][c-1])
@@ -34,7 +38,7 @@ const minPathSum = function(grid) {
       }
     }
   }
-  return minMatrix[minMatrix.length - 1][minMatrix[0].length - 1]
+  return minMatrix[row - 1][column - 1]
 }
 
 // Input:
@@ -53,3 +57,34 @@ const minPathSum = function(grid) {
 // ]
 // Output: 7
 // NOTE: original edge check: boolean if current spot had a spot to the left of it was if(grid[r][c-1]); 0 is a falsy value so with an array of 0's statement will always return false. updated to: if(grid[r][c-1] !== undefined)
+
+//solution: create top row and first column seperately
+//loop through remaining matrix spots
+function minPathSum(grid) {
+  const rows = grid.length
+  const columns = grid[0].length
+  const minMatrix = grid.slice()
+
+  //make copy of array, rather than nested for loop to create another
+  // for(let r = 0; r < rows; r++) {
+  //   let row = []
+  //   for(let c = 0; c < columns; c++) {
+  //     row.push(grid[r][c])
+  //   }
+  //   minMatrix.push(row)
+  // }
+  for(let i = 1; i < columns; i++) {
+    minMatrix[0][i] += minMatrix[0][i - 1]
+  }
+  for(let j = 1; j < rows; j++) {
+    minMatrix[j][0] += minMatrix[j - 1][0]
+  }
+  for(let n = 1; n < rows; n++) {
+    for(let m = 1; m < columns; m++) {
+      let current = minMatrix[n][m]
+      minMatrix[n][m] = Math.min((current + minMatrix[n - 1][m]), (current + minMatrix[n][m - 1]))
+    }
+  }
+  return minMatrix[rows - 1][columns - 1]
+}
+
